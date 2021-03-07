fd5b97cdb903281d739a532047c56fc29f5bfa23
Larren Canapi <42918944+larrencanapi@users.noreply.github.com>
2021-03-06 18:04:25 -0800

    Create keep-alive.py

---
+++ keep-alive.py
@@ -0,0 +1,17 @@
+# keeps the bot running active so it will not sleep
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hello. I am alive!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
